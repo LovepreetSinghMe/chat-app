634c66c6b93802923a5a97ccf87380f987595535
Lovepreet Singh <lovep1730@gmail.com>
2018-11-26 21:32:49 +0530

    Initial commit.

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname + '/../public');
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+});
