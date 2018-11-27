7988dba841a6e0766e395c2b37327e32c81f325e
Lovepreet Singh <lovep1730@gmail.com>
2018-11-27 12:04:37 +0530

    Added form.

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ let socket = io();
 
 socket.on('connect',function () {
     console.log('connected to server.');
-    
+
 });
 
 socket.on('disconnect', function() {
@@ -11,4 +11,19 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('new message recieved', message);
+
+    $('#messages').append(`<li>${message.from}: ${message.text}</li>`);
+});
+
+$(function() {
+    $('#message-form').on('submit', function(e) {
+        e.preventDefault();
+
+        socket.emit('createMessage', {
+            from: 'User',
+            text: $('[name=message]').val()
+        }, function() {
+
+        });
+    });
 });
--- server/server.js
+++ server/server.js
@@ -25,12 +25,6 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         io.emit('newMessage', generateMessage(message.from, message.text));
-
-        // socket.broadcast.emit('newMessage',{
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
     });
 
     socket.on('disconnect', () => {
