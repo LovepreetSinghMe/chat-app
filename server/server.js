ef535171b4f1aff719314bcce1a19740eb233435
Lovepreet Singh <lovep1730@gmail.com>
2018-11-27 11:06:23 +0530

    Greet new user and broadcast to others.

--- server/server.js
+++ server/server.js
@@ -17,12 +17,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app.',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user connected',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
-        io.emit('newMessage',{
+        io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage',{
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
